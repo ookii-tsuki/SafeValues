399b0f17426c761c6cc57d5b953beda2ad66a70f
Phantom <P_hanto_m@outlook.com>
2022-08-13 13:45:00 +0400

    Remove Random object creation from constructors Add static Random field Move structs to separated fiels

---
+++ SafeValues/SafeInt.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace Med.SafeValue
+{
+    /// <summary>
+    /// Hides an integer from cheating tools by making an offset to the value.
+    /// </summary>
+    public struct SafeInt
+    {
+        private int offset;
+        private int value;
+        private static Random rand = new Random();
+
+        public SafeInt(int value = 0)
+        {
+            offset = rand.Next(-1000, 1000);
+            this.value = value + offset;
+        }
+
+        /// <summary>
+        /// The value of this instance.
+        /// </summary>
+        public int Value
+        {
+            get => value - offset;
+            set => this = new SafeInt(value);
+        }
+
+        /// <summary>
+        /// Resets the value and the offset to 0.
+        /// </summary>
+        public void Reset()
+        {
+            offset = 0;
+            value = 0;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+        #region Operators
+        public static SafeInt operator +(SafeInt i1, SafeInt i2)
+        {
+            return new SafeInt(i1.Value + i2.Value);
+        }
+        public static SafeInt operator +(SafeInt i1, SafeFloat f2)
+        {
+            return new SafeInt((int)Math.Round(i1.Value + f2.Value));
+        }
+        public static SafeInt operator ++(SafeInt i1)
+        {
+            return new SafeInt(i1.Value + 1);
+        }
+        public static SafeInt operator -(SafeInt i1, SafeInt i2)
+        {
+            return new SafeInt(i1.Value - i2.Value);
+        }
+        public static SafeInt operator -(SafeInt i1, SafeFloat f2)
+        {
+            return new SafeInt((int)Math.Round(i1.Value - f2.Value));
+        }
+        public static SafeInt operator --(SafeInt i1)
+        {
+            return new SafeInt(i1.Value - 1);
+        }
+        public static SafeInt operator /(SafeInt i1, SafeInt i2)
+        {
+            return new SafeInt(i1.Value / i2.Value);
+        }
+        public static SafeInt operator /(SafeInt i1, SafeFloat f2)
+        {
+            return new SafeInt((int)Math.Round(i1.Value / f2.Value));
+        }
+        public static SafeInt operator *(SafeInt i1, SafeInt i2)
+        {
+            return new SafeInt(i1.Value * i2.Value);
+        }
+        public static SafeInt operator *(SafeInt i1, SafeFloat f2)
+        {
+            return new SafeInt((int)Math.Round(i1.Value * f2.Value));
+        }
+        public static SafeInt operator %(SafeInt i1, SafeInt i2)
+        {
+            return new SafeInt(i1.Value % i2.Value);
+        }
+        public static SafeInt operator %(SafeInt i1, SafeFloat f2)
+        {
+            return new SafeInt((int)Math.Round(i1.Value % f2.Value));
+        }
+        public static bool operator ==(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value == i2.Value;
+        }
+        public static bool operator !=(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value != i2.Value;
+        }
+        public static bool operator ==(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value == f2.Value;
+        }
+        public static bool operator !=(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value != f2.Value;
+        }
+        public static bool operator ==(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value == l2.Value;
+        }
+        public static bool operator !=(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value != l2.Value;
+        }
+        public static bool operator >(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value > i2.Value;
+        }
+        public static bool operator <(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value < i2.Value;
+        }
+        public static bool operator >=(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value >= i2.Value;
+        }
+        public static bool operator <=(SafeInt i1, SafeInt i2)
+        {
+            return i1.Value <= i2.Value;
+        }
+        public static bool operator >(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value > f2.Value;
+        }
+        public static bool operator <(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value < f2.Value;
+        }
+        public static bool operator >=(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value >= f2.Value;
+        }
+        public static bool operator <=(SafeInt i1, SafeFloat f2)
+        {
+            return i1.Value <= f2.Value;
+        }
+        public static bool operator >(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value > l2.Value;
+        }
+        public static bool operator <(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value < l2.Value;
+        }
+        public static bool operator >=(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value >= l2.Value;
+        }
+        public static bool operator <=(SafeInt i1, SafeLong l2)
+        {
+            return i1.Value <= l2.Value;
+        }
+        #endregion
+    }
+}
